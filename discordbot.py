89c0c8d47b7ea24d988052ca3f80dd4a620bd6c3
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-02 17:01:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,7 +21,107 @@ async def ping(ctx):
 async def jin(ctx):
     await ctx.send(':sunglasses: ') 
     
+    
+@bot.command()
+async def yukey(ctx):
+    await ctx.send(':toilet: ') 
+    
+    
+@bot.command()
+async def yuu(ctx):
+    await ctx.send(':poop: ') 
+    
+    
+@bot.command()
+async def noki(ctx):
+    await ctx.send(':cupid: ')   
+    
+    
+@bot.command()
+async def gyu(ctx):
+    await ctx.send(':fingers_crossed:  ')     
+    
+
+@bot.command()
+async def jincome(ctx):
+    await ctx.send('@566444794734444555早く来いよ ') 
+    
+    
+@bot.command()
+async def yukeycome(ctx):
+    await ctx.send('@579665819445887006早く来いよ ')     
+            
+    
+@bot.command()
+async def yuucome(ctx):
+    await ctx.send('@588781728257015821早く来いよ ')     
+    
+    
+@bot.command()
+async def nokicome(ctx):
+    await ctx.send('@579665819445887006早く来いよ ')    
+    
+@bot.command()
+async def gyucome(ctx):
+    await ctx.send('@605025536867303434早く来いよ ') 
+    
+    
+if not discord.opus.is_loaded():
+    discord.opus.load_opus("heroku-buildpack-libopus")
+
+@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    voice_state = ctx.author.voice
+
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("先にボイスチャンネルに入っている必要があります。")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    print("connected to:",channel.name)
 
 
+@bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """Botをボイスチャンネルから切断します。"""
+    voice_client = ctx.message.guild.voice_client
 
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断しました。")
+
+
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")    
+    
+   
+    
+    
+    
+    
+    
+    
 bot.run(token)
