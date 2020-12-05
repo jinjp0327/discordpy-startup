d2fb2ff71184a731aee7648b8b8587cfd3415037
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-06 01:35:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -69,10 +69,6 @@ async def gyucome(ctx):
 async def mac(ctx):
     await ctx.send(':hamburger:')    
     
-@bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send('{0} joined on {0.joined_at}'.format(member))    
-    
 
     
     
