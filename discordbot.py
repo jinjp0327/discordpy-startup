636a8179c79f928cfeb9da0942af5e04a9eeb53d
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-06 02:54:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,9 +11,7 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
-async def on_ready():
-await bot.change_presence(activity=discord.Game(f"Yukey izime")
+await client.change_presence(activity=discord.Game(name='ゆーけーいじめ'))
 
 
 @bot.command()
