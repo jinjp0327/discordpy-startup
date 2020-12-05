51f024a632146032775397e72008f22f8f10587d
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-06 02:47:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,8 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-activity = discord.Activity(name='yukey izime', type=discord.ActivityType.watching)
-await client.change_presence(activity=activity)
+await client.change_presence(activity=discord.Game(name='yukey izime'))
+
 
 @bot.command()
 async def ping(ctx):
