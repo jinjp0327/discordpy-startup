15e55710e6add6dd918b8f7b2ea38036ef09627b
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-06 01:54:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -67,7 +67,21 @@ async def gyucome(ctx):
     
 @bot.command()
 async def mac(ctx):
-    await ctx.send(':hamburger:')    
+    await ctx.send(':hamburger:')
+    
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.commands_heading = "コマンド:"
+        self.no_category = "その他"
+        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+
+    def get_ending_note(self):
+        return (f"各コマンドの説明: {prefix}help <コマンド名>\n"
+                f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
+
+
+bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())    
     
 
     
