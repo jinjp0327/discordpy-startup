3fe0adec4de87e0425ece4446a345e642916c6e2
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-07 23:35:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -91,18 +91,6 @@ async def boss(ctx):
 async def anarisuto(ctx):
     await ctx.send('<@&784767086752169984>')
 		   
-		   
-
- 
-@bot.command(name='server')
-async def fetchServerInfo(context):
-	guild = context.guild
-	
-	await context.send(f'server Name: {guild.name}')
-	await context.send(f'server Size: {len(guild.members)}')
-	await context.send(f'Server Name: {guild.owner.display_name}') 
-	
-
 
     
 bot.run(token)
