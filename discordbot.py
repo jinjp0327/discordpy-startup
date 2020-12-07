2facbc264d986ed4e1ac890d4cb0016798fe8346
jinjp0327 <58094605+jinjp0327@users.noreply.github.com>
2020-12-07 13:46:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,18 +6,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
-
-@bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
-
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
 @bot.cmmands()
 async def cat(ctx):
     await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
